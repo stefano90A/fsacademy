e4f098c5cf1421c2c0a184da94b0e53d05624519
Stefano Romano <stefano.romano@finconsgroup.com>
2022-10-19 10:16:55 +0200

    User Card

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -22,38 +22,7 @@
             
         </b-navbar>
 
-        <div>
-            <b-card
-                no-body
-                style="max-width: 80%;"
-                v-if="employee.name && show=='userProfile'"
-                class="m-auto"
-            >
-                <template #header>
-                    <h4 class="mb-0">Utente: {{employee.name}} {{employee.surname}}</h4>
-                </template>
-
-                <b-card-body align="left">
-                <b-card-title>Nome: {{employee.name}}</b-card-title>
-                <b-card-sub-title class="mb-2">Cognome: {{employee.surname}}</b-card-sub-title>
-                <b-container class="bv-example-row">
-                    <b-row class="mb-3">
-                    <b-col class="fontBold">Email: </b-col>
-                    <b-col>{{employee.email}}</b-col>
-                    <b-col class="fontBold">Sede Appartenenza: </b-col>
-                    <b-col>{{employee.location.name}}</b-col>
-                    </b-row>
-                    <b-row class="mb-3">
-                    <b-col class="fontBold">Tipo Contratto: </b-col>
-                    <b-col>{{employee.member.name}}</b-col>
-                    <b-col class="fontBold">Trattamento: </b-col>
-                    <b-col>{{employee.refund.name}}</b-col>
-                    </b-row>
-                </b-container>
-                
-                </b-card-body>
-            </b-card>
-        </div>
+        <UserCard v-if="show=='userProfile'" :employee="employee" />
 
         <UserManager v-if="show=='userManager'" ></UserManager>
 
@@ -63,6 +32,7 @@
 <script>
 import axios from "axios";
 import UserManager from "./UserManager.vue"
+import UserCard from "./ProfileCard.vue"
 
 export default {
     name: "HomePage",
@@ -70,7 +40,8 @@ export default {
         userId: String,
     },
     components: {
-        UserManager
+        UserManager,
+        UserCard
     },
     data() {
         return {
